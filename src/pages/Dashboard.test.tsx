eedd24ee191ee927a383b6fdc6a5f32c80f7f3be
Vedrana Djukic <rukeudzepovima@gmail.com>
2023-03-08 13:26:25 -0500

    13-unit-test-failing: add shareholder fix

--- src/pages/Dashboard.test.tsx
+++ src/pages/Dashboard.test.tsx
@@ -303,6 +303,6 @@ describe("Dashboard", () => {
     await waitFor(() => expect(shareholderNameInput).not.toBeVisible());
     expect(
       await screen.findByTestId("shareholder-Mike-group")
-    ).toHaveTextContent("employee");
+    ).toHaveTextContent("investor");
   }, 10000);
 });
